7cd041c521c43b0c3a55d444976132b15609683e
lukashsasha <67514248+lukashsasha@users.noreply.github.com>
2023-08-02 17:59:06 +0300

    1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("scroll", e => {
+    document.body.style.cssText += `--scrollTop: ${window.scrollY}px;`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
